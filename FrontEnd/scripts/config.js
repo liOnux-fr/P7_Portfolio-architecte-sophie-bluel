34629d6411c3cfc4b2a6d4d2866edc555d8704db
andy123-glitch <andy.azerot@gmail.com>
2025-11-20 13:51:57 +0100

    filter hover

--- FrontEnd/scripts/config.js
+++ FrontEnd/scripts/config.js
@@ -1,6 +1,6 @@
 const urlAPI = "http://localhost:5678/api";
 export const POSTlogin = urlAPI + "/users/login"
-export const GETcategories = urlAPI + "/categories";
+export const GETcategory = urlAPI + "/categories";
 export const GETworks = urlAPI + "/works";
 export const POSTworks = urlAPI + "/works";
 export const DELETEworks = urlAPI + "/works/";
--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,13 +1,13 @@
-import { GETworks } from "/scripts/config.js";
+import { GETworks, GETcategory } from "/scripts/config.js";
 
 export async function displayWorks() {
     //Get works by API
     const works = await fetch(GETworks).then((works) => works.json());
     let figure = "";
-    dispaly(works)
+    dispaly(works);
 }
 
-function dispaly(array){
+function dispaly(array) {
     let figure = "";
     // Loop in works for add html node in figure to add inner div.gallery
     array.forEach((element) => {
@@ -19,17 +19,32 @@ function dispaly(array){
     document.querySelector(".gallery").innerHTML = figure;
 }
 
-export function filters(){
+export async function filters() {
+    const divFilters = document.querySelector(".filters");
+    const category = await fetch(GETcategory).then((category) => category.json());
+    category.forEach((c) =>{
+        let btn = `<button id="${c.id}">${c.name}</button>`;
+        divFilters.innerHTML += btn;
+    })
+
     const filterBtn = document.querySelectorAll(".filters");
-    filterBtn.forEach(btn => {
+
+    filterBtn.forEach((btn) => {
         btn.addEventListener("click", async (e) => {
-            const id = e.target.dataset.id;
+            const filterBtns = document.querySelectorAll(".filters button");
+            filterBtns.forEach((btn) => {
+                btn.classList.remove("active");
+            });
+            e.target.classList.add("active");
+
+            const id = e.target.id;
+        console.log(id);
+
             const works = await fetch(GETworks).then((works) => works.json());
             if (id != 0) {
                 const filteredBtn = works.filter((element) => element.categoryId == id);
                 dispaly(filteredBtn);
             } else dispaly(works);
         });
-        
     });
 }
