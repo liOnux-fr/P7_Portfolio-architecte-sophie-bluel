7564af5cbed980bfdf8c6d19c1782227aa6350e1
liOnux <lio07@mailo.com>
2025-12-23 18:22:37 +0100

    Etape 4 : reste btn_selected

--- FrontEnd/assets/gallery.js
+++ FrontEnd/assets/gallery.js
@@ -1,59 +1,45 @@
 const filters = document.querySelector("#filter-button");
 const gallery = document.querySelector(".gallery");
+const urlAPI = "http://localhost:5678/api/"
 
 function majGallery(id) {
-fetch("http://localhost:5678/api/works")
-	.then(response => response.json())
-	.then(datas => {
-		datas.forEach(element => {
-			const baliseFigure = document.createElement("figure");
-            const baliseFigcaption = document.createElement("figcaption");
-            const baliseImg = document.createElement("img");
-			gallery.appendChild(baliseFigure);
-			baliseFigure.appendChild(baliseImg);
-			baliseImg.src = element.imageUrl;
-			baliseImg.alt = element.title;
-			baliseFigure.appendChild(baliseFigcaption);
-			baliseFigcaption.textContent = element.title;
+	gallery.innerHTML = "";
+	fetch(`${urlAPI}works`)
+		.then(response => response.json())
+		.then(works => {
+			if (id > 0) {
+				works = works.filter(element => element.categoryId === id);
+			}
+			works.forEach(element => {
+				const baliseFigure = document.createElement("figure");
+				const baliseFigcaption = document.createElement("figcaption");
+				const baliseImg = document.createElement("img");
+				baliseImg.src = element.imageUrl;
+				baliseImg.alt = element.title;
+				baliseFigcaption.textContent = element.title;
+				baliseFigure.append(baliseImg, baliseFigcaption);
+				gallery.appendChild(baliseFigure);
+			})
 		})
-	})
-}
+};
 
-majGallery();
+majGallery(0);
 
-fetch("http://localhost:5678/api/categories")
+fetch(`${urlAPI}categories`)
 	.then(response => response.json())
 	.then(categories => {
-		if (categories.length < 2) {
-			return;
-		}
+		filters.innerHTML = "";
+		if (categories.length < 2) return;
 		filters.innerHTML = `<button class="buttons" type="button" data-id="0">Tous</button>`;
-		const tousBtn = document.querySelector("#filter-button .buttons");
-		tousBtn.addEventListener("click", function() {
-			console.log(tousBtn.dataset.id);
-			majGallery(tousBtn.dataset.id);
-		});
+		const TousBtn = document.querySelector("#filter-button .buttons");
+		TousBtn.addEventListener("click", () => majGallery(0));
 		categories.forEach(element => {
             const baliseBtn = document.createElement("button");
-			filters.appendChild(baliseBtn);
 			baliseBtn.classList.add("buttons");
 			baliseBtn.type = "button";
 			baliseBtn.textContent = element.name;
 			baliseBtn.dataset.id = element.id;
-			baliseBtn.addEventListener("click", function() {
-				console.log(baliseBtn.dataset.id);
-				majGallery(baliseBtn.dataset.id);
-			});
+			baliseBtn.addEventListener("click", () => majGallery(element.id));
+			filters.appendChild(baliseBtn);
 		});
 	});
-	
-// buttons.forEach(btn => {
-// 	buttons.addEventListener("click", function() {
-// 		console.log(baliseBtn.dataset.id)
-// 	})
-// })
-
-// let filtersTab = []
-// filtersTab = Array.from(categories);
-// console.log(filtersTab[2]);
-
